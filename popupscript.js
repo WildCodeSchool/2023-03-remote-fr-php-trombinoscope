03f5127c209b64ca9cde947f481cf7bf728314ef
BrianV87 <Brian.v87@hotmail.fr>
2023-03-17 11:38:00 +0100

    ajout des plusieurs fonctio popup + bare nav page2

--- popupscript.js
+++ popupscript.js
@@ -20,6 +20,47 @@ for (let i = 0; i < photos.length; i++) {
 }
 
 
+function navigatePopup(direction, event) {
+  let currentIndex = -1;
+
+  for (let i = 0; i < photos.length; i++) {
+    if (photos[i].parentElement.nextElementSibling.classList.contains("active")) {
+      currentIndex = i;
+      break;
+    }
+  }
+
+  if (currentIndex >= 0) {
+    let newIndex = currentIndex + direction;
+    if (newIndex < 0) newIndex = photos.length - 1;
+    if (newIndex >= photos.length) newIndex = 0;
+
+    photos[currentIndex].parentElement.nextElementSibling.classList.toggle("active");
+    photos[newIndex].parentElement.nextElementSibling.classList.toggle("active");
+  }
+}
+
+let prevButtons = document.querySelectorAll(".prev-button");
+let nextButtons = document.querySelectorAll(".next-button");
+
+for (let i = 0; i < prevButtons.length; i++) {
+  prevButtons[i].addEventListener("click", function(event) {
+    event.stopPropagation(); // Ajoutez cette ligne
+    navigatePopup(-1, event);
+  });
+  nextButtons[i].addEventListener("click", function(event) {
+    event.stopPropagation(); // Ajoutez cette ligne
+    navigatePopup(1, event);
+  });
+}
+
+
+
+
+
+
+
+
 let JF = document.querySelector(".JF");
 let JF_container = document.querySelector(".JF_container");
 JF.addEventListener("click", function(){
